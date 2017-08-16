6a0109ff1f58e2408a7a3997c04da3b721a2f06d
Pavel Viznitsa <viznitsap@gmail.com>
2017-08-17 00:30:28 +0300

    add if statment_ver_0.0.1

--- js/main.js
+++ js/main.js
@@ -10,48 +10,48 @@ function ress() {
 
     if (gbo_price == 0 || benz_price == 0 || gaz_price == 0 || rashod_benz == 0 || probeg_mes == 0) {
         alert('Не хватает данных для выполнения расчета!');
+        return false;
+    } else {
+        // Расход газа на 100км.
+        var rashod_gaz = rashod_benz * 1.2;
+
+        // Затраты на приобретения бензина на 100км., грн.:
+        var b = rashod_benz * benz_price;
+
+        // Затраты на приобретения газа на 100км.:
+        var g = rashod_gaz * gaz_price;
+
+        // Разница затраченных денег на топливо на 100км., грн.:
+        var r = b - g;
+
+        // Затраты на 1км., при использовании газа, составляет, грн.:
+        var km = r / 100;
+
+        // Экономия на газу за месяц, грн.:
+        var mes = km * probeg_mes;
+
+        // Экономия на газу за год, грн.:
+        var god = mes * year;
+
+        // Время окупаемости ГБО, месяцев, грн.:
+
+        var gbo_res_mes = (gbo_price / km) / probeg_mes;
+
+        console.log(gbo_price);
+        console.log(rashod_gaz);
+        console.log(b);
+        console.log(g);
+        console.log(r);
+        console.log(km);
+        console.log(mes);
+        console.log(god);
+        console.log(gbo_res_mes);
+
+
+        document.getElementById('res_gbo').innerHTML = gbo_res_mes;
+
+        document.getElementById('eco_month').innerHTML = mes;
+
+        document.getElementById('eco_year').innerHTML = god;
     }
-
-
-    // Расход газа на 100км.
-    var rashod_gaz = rashod_benz * 1.2;
-
-    // Затраты на приобретения бензина на 100км., грн.:
-    var b = rashod_benz * benz_price;
-
-    // Затраты на приобретения газа на 100км.:
-    var g = rashod_gaz * gaz_price;
-
-    // Разница затраченных денег на топливо на 100км., грн.:
-    var r = b - g;
-
-    // Затраты на 1км., при использовании газа, составляет, грн.:
-    var km = r / 100;
-
-    // Экономия на газу за месяц, грн.:
-    var mes = km * probeg_mes;
-
-    // Экономия на газу за год, грн.:
-    var god = mes * year;
-
-    // Время окупаемости ГБО, месяцев, грн.:
-
-    var gbo_res_mes = (gbo_price / km) / probeg_mes;
-
-    console.log(gbo_price);
-    console.log(rashod_gaz);
-    console.log(b);
-    console.log(g);
-    console.log(r);
-    console.log(km);
-    console.log(mes);
-    console.log(god);
-    console.log(gbo_res_mes);
-
-
-    document.getElementById('res_gbo').innerHTML = gbo_res_mes;
-
-    document.getElementById('eco_month').innerHTML = mes;
-
-    document.getElementById('eco_year').innerHTML = god;
 }
